6e32fffcdffa3d47265a5db42094b2e9ddc38a0d
APSMaini <apsmaini@yahoo.com>
2020-03-23 15:55:59 +0530

    changes to App.js"

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p><h3>Changes tested successfully</h3></p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Part 2.
+          Edit <code>src/App.js</code> and save to reload. Part 3.
         </p>
         <a
           className="App-link"
